b0efaa02f0fab07bd5683b73f09514742164c4cf
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-22 14:21:07 -0300

    obtendo dados do usuário

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,16 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useState } from 'react';
 import Router from 'next/router';
 
 import { firebase, auth } from '../services/firebase'
 
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 type AuthContextData = {
+  user: User;
   isAuthenticated: boolean;
   signInWithGoogle: () => void;
 }
@@ -17,12 +24,29 @@ export const AuthContext = createContext({} as AuthContextData);
 export function AuthProvider({
   children
 }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
 
   function signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider).then(result => {
       console.log(result)
+      if (result.user) {
+        const {
+          displayName,
+          photoURL,
+          uid
+        } = result.user;
+        if (!displayName || !photoURL) {
+          throw new Error('Missing information from Google Account.')
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL,
+        })
+      }
       Router.push('/rooms/new')
     })
   }
