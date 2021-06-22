0b12d0d74f4b6669522a625b71df821cc239762e
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-22 10:19:55 -0300

    criando layout da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,47 @@
+import Image from 'next/image'
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
-    <h1>Hello NLW</h1>
+    <div className={styles.pageAuth}>
+      <aside>
+        <Image
+          src="/illustration.svg"
+          alt="Ilustração simbolizando perguntas e respostas"
+          width="400px"
+          height="100vh"
+        />
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div>
+          <Image
+            src="/logo.svg"
+            alt="Letmeask"
+            width="400px"
+            height="100vh"
+          />
+          <button>
+            <Image
+              src="/google-icon.svg"
+              alt="Logo do Google"
+              width="400px"
+              height="100vh"
+            />
+            Crie sua sala com o Google
+          </button>
+          <div>ou entre em uma sala</div>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <button type="submit">Entrar na sala</button>
+          </form>
+        </div>
+      </main>
+    </div>
   )
 }
